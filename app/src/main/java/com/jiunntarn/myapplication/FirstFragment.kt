f12776b6460b79a90d863abe3cd3224ff350afbc
JiunnTarn <aencil@yeah.net>
2024-02-20 19:54:40 +0800

    initial commit

---
+++ app/src/main/java/com/jiunntarn/myapplication/FirstFragment.kt
@@ -0,0 +1,67 @@
+package com.jiunntarn.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jiunntarn.myapplication.databinding.FragmentFirstBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            val client = OkHttpClient()
+
+            val url = "http://jiunntarn.com/put"
+            val body = "test".toRequestBody("text/plain".toMediaTypeOrNull())
+
+            val request: Request = Request.Builder().url(url).put(body).build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.e("TAG", "Request url: ${response.request.url}")
+                    Log.e("TAG", "Response: ${response.code} ${response.message}")
+                }
+            })
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
